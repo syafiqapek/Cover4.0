ddb2d63020d363c6627b78b261df687145daf745
Nurhaifaa <nurhaifaatumiran@gmail.com>
2017-09-07 09:58:15 +0800

    latest edit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -8,22 +8,39 @@ import { ListPage } from '../pages/list/list';
 
 import { StatusBar } from '@ionic-native/status-bar';
 import { SplashScreen } from '@ionic-native/splash-screen';
+import { ReportPage } from "../pages/report/report";
+import { HistoryPage } from "../pages/history/history";
+import { SchedulePage } from "../pages/schedule/schedule";
+import { ProfilePage } from "../pages/profile/profile";
+import { ModalPageModule } from "../pages/modal/modal.module";
+import { LoginPage } from "../pages/login/login";
 
 @NgModule({
   declarations: [
     MyApp,
     HomePage,
-    ListPage
+    ListPage,
+    ReportPage,
+    HistoryPage,
+    SchedulePage,
+    ProfilePage,
+    LoginPage
   ],
   imports: [
     BrowserModule,
     IonicModule.forRoot(MyApp),
+    ModalPageModule,
   ],
   bootstrap: [IonicApp],
   entryComponents: [
     MyApp,
     HomePage,
-    ListPage
+    ListPage,
+    ReportPage,
+    HistoryPage,
+    SchedulePage,
+    ProfilePage,
+    LoginPage
   ],
   providers: [
     StatusBar,
